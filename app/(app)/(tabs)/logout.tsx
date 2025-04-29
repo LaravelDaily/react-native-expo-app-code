37826b19b45b34192081e4e2ff7062fad50f4d8b
Modestas Vaitkevičius <info@modestasv.lt>
2025-04-29 13:09:47 +0300

    Little update to match the lesson

--- app/(app)/(tabs)/logout.tsx
+++ app/(app)/(tabs)/logout.tsx
@@ -1,13 +1,11 @@
 import { Redirect, useFocusEffect } from 'expo-router';
 
-import { useSession } from '../../../context/ctx';
+import { useSession } from '@/context/ctx';
 
 export default function AppLayout() {
   const { signOut } = useSession();
 
-  useFocusEffect(() => {
-    signOut();
-  })
+  signOut();
 
   return <Redirect href="/sign-in" />;
 }
